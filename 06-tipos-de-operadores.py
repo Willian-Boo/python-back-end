28314f765a8e64483aa0f84586089d422f8ae4e0
Willian <willianboo@outlook.com>
2025-11-04 22:10:11 -0300

    operadores de comparação e atribuição

---
+++ 06-tipos-de-operadores.py
@@ -0,0 +1,18 @@
+valor_1 = "10"
+valor_2 = 10
+
+#igualdade ==
+print(valor_1 == valor_2)
+# retorno será false
+
+#maior >
+print(int(valor_1) > valor_2)
+
+#maior ou igual >=
+print(int(valor_1) >= valor_2)
+
+#menor <
+print(int(valor_1) < valor_2)
+
+#menor ou igual <=
+print(int(valor_1) <= valor_2)
---
+++ 07-operadores-atribuicao.py
@@ -0,0 +1,14 @@
+saldo = 500.00
+print(saldo)
+
+saldo += 30.75
+print(saldo)
+
+saldo -= 60.5
+print(saldo)
+
+saldo *= 2
+print(saldo)
+
+saldo /= 3
+print(saldo)
