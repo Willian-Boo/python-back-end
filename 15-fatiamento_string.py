b107dac6142cd5516ed16304e24953f9ca5ba2df
Willian <willianboo@outlook.com>
2025-11-17 21:36:46 -0300

    Fatiamento e strings e multiplas linhas

---
+++ 15-fatiamento_string.py
@@ -0,0 +1,25 @@
+nome = "Willian Borges Silva"
+
+#exibir apenas a primeira letra
+print(nome[0])
+
+#exibir a última letra
+print(nome[-1])
+
+#exibir do inicio do texto até 4 letras seguintes
+print(nome[:4])
+
+#começa a exibir o texto após 7 letras
+print(nome[7:])
+
+#passando intervalos de inicio e fim
+print(nome[8:14])
+
+#intervalo de inicio e fim, com step de 2 em 2
+print(nome[8:14:2])
+
+#retorna uma cópia da string
+print(nome[:])
+
+#retorna o texto invertido
+print(nome[::-1])
---
+++ 16-multiplas_linhas.py
@@ -0,0 +1,6 @@
+mensagem = '''   Esse aqui é um texto com multiplas linhas
+         é possível deixar com esses espaços       no texto 
+    Veja o exemplo
+'''
+
+print(mensagem)
