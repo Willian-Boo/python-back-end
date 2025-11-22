704159428c63cfdcf3569338c5b7125701e81a6a
Willian <willianboo@outlook.com>
2025-11-22 14:53:03 -0300

    trabalhando com listas

---
+++ 17-listas.py
@@ -0,0 +1,33 @@
+frutas = ["laranja", "maça", "uva"]
+
+letras = list("python")
+
+#acesso direto na lista
+frutas[0]
+print(frutas[0]) # "laranja"
+print(frutas[-1]) # "uva"
+
+#fatiamento
+print(letras[:2])
+
+#append adiciona itens na lista
+frutas.append("banana")
+print(frutas)
+
+#clear limpa a lista
+
+#copy faz uma cópia da lista
+
+#count faz cotagem dos elementos
+
+#extend junta a lista
+
+#index, encontra o indice da lista
+
+#pop, retira o elemento de uma lista
+
+#remove, retira o elemento passando o valor do item
+
+#reverse, inverte a ordem
+
+#sort, ordenar a lista
